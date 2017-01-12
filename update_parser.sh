3d25eef3ada505c6de78fe4a99b4d9aa76d886d4
Tim Ryan <id@timryan.org>
2017-01-11 22:42:42 -0800

    Removes compression step from lalrpop.

--- update_parser.sh
+++ update_parser.sh
@@ -5,10 +5,5 @@ set -e
 cd $(dirname $0)
 cd src
 
-rm verilog_parser.rs 2>/dev/null || true
-rm verilog_parser.rs.gz 2>/dev/null || true
-
 lalrpop verilog_parser.lalrpop
 chmod 0644 verilog_parser.rs
-
-gzip verilog_parser.rs
